71f342c1bfb54825df2059d904c98fb9c862c931
Oliver Rümpelein <oli_r@fg4f.de>
2015-04-18 12:42:03 +0200

    Changed output to match JSON

--- plugins/arch.py
+++ plugins/arch.py
@@ -28,25 +28,28 @@ returns: dictionary containing the JSON-Data
 		if args=="":
 			return "Please specify a keyword."
 		query_content=self.__query_api(args,"name")
-		if query_content["resultcount"]==1:
-			query_package=query_content["results"]
-			return query_package["Name"] + ":\n" + query_package["Description"]
-		else:
+		query_packages=query_content["results"]
+		if len(query_packages)==0:
 			return args + " was not found, or something else went wrong. Sorry."
-	
+		else:
+			return query_packages["pkgname"] + ":\n" + query_packages["pkgdesc"]
+
+
 	@botcmd
 	def arch_search(self,msg,args):
 		"""Searches for Arch packages"""
 		if args=="":
 			return "Please specify a keyword."
 		query_content=self.__query_api(args,"q")
-		if query_content["resultcount"] == 0:
+		query_packages=query_content["results"]
+		if len(query_packages) == 0:
 			return "No package matching your query found"
 		else:
-			query_str= str(query_content["resultcount"]) + " matching packages found.\n"
-			for query_elem in query_content["results"]:
-				query_str+= query_elem["Name"] + ":\t" + query_elem["Description"]+"\n"
+			query_str= str(len(query_packages)) + " matching packages found.\n"
+			for query_elem in query_packages:
+				query_str+= query_elem["repo"]+"/"+query_elem["pkgname"] + ":\t" + query_elem["pkgdesc"]+"\n"
 			return query_str
+
 
 	@botcmd
 	def arch_maint(self,msg,args):
@@ -54,10 +57,11 @@ returns: dictionary containing the JSON-Data
 		if args=="":
 			return "Please specify a keyword."
 		query_content=self.__query_api(args,"maintainer")
-		if query_content["resultcount"] == 0:
+		query_packages=query_content["results"]
+		if len(query_packages) == 0:
 			return "No packages maintained by "+args+" found."
 		else:
-			query_str= str(query_content["resultcount"]) + " packages maintained by "+args+" found.\n"
-			for query_elem in query_content["results"]:
-				query_str += query_elem["Name"] + ":\t" + query_elem["Description"]+"\n"
+			query_str= str(len(query_packages)) + " packages maintained by "+args+" found.\n"
+			for query_elem in query_packages:
+				query_str += query_elem["repo"]+"/"+query_elem["pkgname"] + ":\t" + query_elem["pkgdesc"]+"\n"
 			return query_str
